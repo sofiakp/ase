52d42154714be5c2e3ff7684103646b60d546ebf
sofiakp <sofiakp@behemoth2>
2012-08-06 21:16:34 -0700

    Combine reps can select non-technical replicates

--- rscripts/utils/combine.reps.r
+++ rscripts/utils/combine.reps.r
@@ -1,8 +1,10 @@
 rm(list=ls())
+library('foreach')
+library('doMC')
 source(file.path(Sys.getenv('MAYAROOT'), 'src/rscripts/utils/binom.val.r'))
 source(file.path(Sys.getenv('MAYAROOT'), 'src/rscripts/utils/sample.info.r'))
 
-combine.reps <- function(filenames, outfile, get.q = T){
+combine.reps <- function(filenames, outfile, get.q = T, get.p = T){
   nfiles <- length(filenames)
   for(i in 1:nfiles){
     load(filenames[i])
@@ -26,27 +28,55 @@ combine.reps <- function(filenames, outfile, get.q = T){
   counts <- all.counts
   snp.info <- all.snp.info
   sample <- all.sample
-  mat.idx <- grep('mat', colnames(counts))
-  pat.idx <- grep('pat', colnames(counts))
-  fwd.idx <- grep('fwd', colnames(counts))
-  sb <- binom.val(rowSums(counts[, fwd.idx]), rowSums(counts))
-  pval <- binom.val(rowSums(counts[, mat.idx]), rowSums(counts[, c(mat.idx, pat.idx)]))
-  qval <- array(1, dim = dim(pval))
-  if(get.q) qval[snp.info$pass] = p.adjust(pval[snp.info$pass], method = 'BH')
-  snp.info$qval = qval
-  snp.info$pval = pval
-  snp.info$sb = sb
+  if(length(filenames) > 1){
+    mat.idx <- grep('mat', colnames(counts))
+    pat.idx <- grep('pat', colnames(counts))
+    fwd.idx <- grep('fwd', colnames(counts))
+    sb <- binom.val(rowSums(counts[, fwd.idx]), rowSums(counts))
+    pval <- array(1, dim = c(dim(snp.info)[1], 1))
+    if(get.p) pval <- binom.val(rowSums(counts[, mat.idx]), rowSums(counts[, c(mat.idx, pat.idx)]))
+    qval <- array(1, dim = c(dim(snp.info)[1], 1))
+    if(get.q) qval[snp.info$pass] = p.adjust(pval[snp.info$pass], method = 'BH')
+    snp.info$qval = qval
+    snp.info$pval = pval
+    snp.info$sb = sb
+  }  
   save(counts, snp.info, sample, file = outfile)
 }
 
+select.reps = function(reps){
+  good.reps = array(T, dim = c(length(reps), 1))
+  for(i in 1:5){
+    ci = as.character(i)
+    if(ci %in% reps){
+      good.reps[grep(paste(ci, '\\.', sep = ''), reps)] = F
+    }else if(paste(ci, '1', sep = '.') %in% reps){
+      good.reps[grep(paste(ci, '\\.[2-9]', sep = ''), reps)] = F
+    }
+  }
+  return(good.reps)
+}
+
+registerDoMC(2)
 filenames <- list.files(file.path(Sys.getenv('MAYAROOT'), 'rawdata/alleleCounts/'), 
-                        pattern = '[0-9]+\\.counts\\.r', full.name = T)
+                        pattern = '[0-9]+\\.counts\\.r', full.name = T, recursive = T, include.dirs = F)
+outdir = file.path(Sys.getenv('MAYAROOT'), 'rawdata/alleleCounts/', 'reps_6Aug12')
+if(!file.exists(outdir)) dir.create(outdir)
+
 all.info <- sample.info(filenames, '\\.counts\\.r$')
 types <- unique(all.info[, 1:2])
-for(t in 1:dim(types)[1]){
+overwrite = F
+foreach(t = 1:dim(types)[1]) %dopar%{
   reps <- which(all.info$indiv == types$indiv[t] & all.info$mark == types$mark[t])
-  if(length(reps) > 1){
-    outfile <- gsub('[0-9\\.]+\\.counts.r$', 'rep.counts.r', filenames[reps[1]])
-    combine.reps(filenames[reps], outfile, get.q = T)
+  if(length(reps) > 0){ # set to 0 to create rep files even for cases when there are no replicates
+    print(basename(filenames[reps]))
+    sel.reps = reps[select.reps(all.info$rep[reps])]
+    # Write all the replicates (including technical)
+    #if(length(sel.reps) != length(reps)){
+    #  outfile <- file.path(outdir, gsub('[0-9\\.]+\\.counts.r$', 'reptr.counts.r', basename(filenames[reps[1]])))
+    #  if(!file.exists(outfile) || overwrite) combine.reps(filenames[reps], outfile, get.q = F, get.p = F)
+    #}
+    outfile <- file.path(outdir, gsub('[0-9\\.]+\\.counts.r$', 'rep.counts.r', basename(filenames[reps[1]])))
+    if(!file.exists(outfile) || overwrite) combine.reps(filenames[sel.reps], outfile, get.q = T)
   }
 }
