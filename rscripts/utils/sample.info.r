22c68c0524138ad5c62d83949704f1352a49dc6b
sofiakp <sofiakp@behemoth2>
2012-09-11 16:34:09 -0700

    Fixed problem in sample.info.r with INPUT files

--- rscripts/utils/sample.info.r
+++ rscripts/utils/sample.info.r
@@ -1,5 +1,5 @@
 sample.info <- function(x, suf = '\\.[a-zA-z0-9]*$'){
-  sample <- gsub('INPUT\\.', 'INPUT_1\\.', gsub(paste('SNYDER_HG19_|', suf, sep = ''), '', basename(x), perl = T))
+  sample <- gsub('INPUT$', 'INPUT_1', gsub(paste('SNYDER_HG19_|', suf, sep = ''), '', basename(x), perl = T))
   fields <- unlist(strsplit(sample, '_'))
   if(length(fields) < 3){
     return(NULL)
