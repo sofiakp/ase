3ad7fcbfb7f903c0948dcc2f2673d2af38821b1d
sofiakp <sofiakp@behemoth2>
2013-12-22 00:12:11 -0800

    Added README

--- python/checkFastqFormat.py
+++ python/checkFastqFormat.py
@@ -1,8 +1,13 @@
 import sys
 import fileinput
+import argparse
+
+desc = 'Checks FASTQ format. Reads a FASTQ from STDIN. Outputs 0 if the scores are Phred+33 (Illumina 1.8+) and 1 if the scores are Phred+64 (Illumina 1.5+). Raises an exception if the format cannot be determined (since the two ranges overlap).'
+parser = argparse.ArgumentParser(description = desc)
+args = parser.parse_args()
 
 for idx, line in enumerate(fileinput.input()):
-    if (idx + 1) %  4== 0:
+    if (idx + 1) %  4 == 0:
         for c in line.strip():
             if ord(c) > 74:
                 print 1
